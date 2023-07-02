ca1d143658249de172f4be8f1b2f8d7f6b9f1607
Dariusz Rybicki <darrarski@gmail.com>
2023-07-03 01:12:04 +0200

    Log network requests with Pulse
    Uses experimental feature

--- Example/GoogleDriveClientExampleApp/App.swift
+++ Example/GoogleDriveClientExampleApp/App.swift
@@ -1,8 +1,13 @@
+import Pulse
 import PulseUI
 import SwiftUI
 
 @main
 struct App: SwiftUI.App {
+  init() {
+    Experimental.URLSessionProxy.shared.isEnabled = true
+  }
+
   var body: some Scene {
     WindowGroup {
       TabView {
