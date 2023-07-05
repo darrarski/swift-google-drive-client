cbc4120b3824c69d0274691c303bec9b34586495
Dariusz Rybicki <darrarski@gmail.com>
2023-07-06 01:30:03 +0200

    Add GetFileData tests

---
+++ Tests/GoogleDriveClientTests/GetFileDataTests.swift
@@ -0,0 +1,128 @@
+import XCTest
+@testable import GoogleDriveClient
+
+final class GetFileDataTests: XCTestCase {
+  func testGetFileData() async throws {
+    let fileId = "1234"
+    let credentials = Credentials(
+      accessToken: "access-token-1",
+      expiresAt: Date(),
+      refreshToken: "refresh-token-1",
+      tokenType: "token-type-1"
+    )
+    let httpRequests = ActorIsolated<[URLRequest]>([])
+    let didRefreshToken = ActorIsolated(0)
+    let getFileData = GetFileData.live(
+      auth: {
+        var auth = Auth.unimplemented()
+        auth.refreshToken = {
+          await didRefreshToken.withValue { $0 += 1 }
+        }
+        return auth
+      }(),
+      keychain: {
+        var keychain = Keychain.unimplemented()
+        keychain.loadCredentials = { credentials }
+        return keychain
+      }(),
+      httpClient: .init { request in
+        await httpRequests.withValue { $0.append(request) }
+        return (
+          "file data".data(using: .utf8)!,
+          HTTPURLResponse(
+            url: URL(filePath: "/"),
+            statusCode: 200,
+            httpVersion: nil,
+            headerFields: nil
+          )!
+        )
+      }
+    )
+
+    let data = try await getFileData(fileId: fileId)
+
+    await httpRequests.withValue {
+      let url = URL(string: "https://www.googleapis.com/drive/v3/files/\(fileId)?alt=media")!
+      var expectedRequest = URLRequest(url: url)
+      expectedRequest.httpMethod = "GET"
+      expectedRequest.allHTTPHeaderFields = [
+        "Authorization": "\(credentials.tokenType) \(credentials.accessToken)"
+      ]
+      XCTAssertEqual($0, [expectedRequest])
+    }
+    XCTAssertEqual(data, "file data".data(using: .utf8)!)
+  }
+
+  func testGetFileDataErrorResponse() async {
+    let getFileData = GetFileData.live(
+      auth: {
+        var auth = Auth.unimplemented()
+        auth.refreshToken = {}
+        return auth
+      }(),
+      keychain: {
+        var keychain = Keychain.unimplemented()
+        keychain.loadCredentials = {
+          Credentials(
+            accessToken: "",
+            expiresAt: Date(),
+            refreshToken: "",
+            tokenType: ""
+          )
+        }
+        return keychain
+      }(),
+      httpClient: .init { request in
+        (
+          "Error!!!".data(using: .utf8)!,
+          HTTPURLResponse(
+            url: URL(filePath: "/"),
+            statusCode: 500,
+            httpVersion: nil,
+            headerFields: nil
+          )!
+        )
+      }
+    )
+
+    do {
+      _ = try await getFileData(fileId: "1234")
+      XCTFail("Expected to throw, but didn't")
+    } catch {
+      XCTAssertEqual(
+        error as? GetFileData.Error,
+        .response(
+          statusCode: 500,
+          data: "Error!!!".data(using: .utf8)!
+        ),
+        "Expected to throw response error, got \(error)"
+      )
+    }
+  }
+
+  func testGetFileDataWhenNotAuthorized() async {
+    let getFileData = GetFileData.live(
+      auth: {
+        var auth = Auth.unimplemented()
+        auth.refreshToken = {}
+        return auth
+      }(),
+      keychain: {
+        var keychain = Keychain.unimplemented()
+        keychain.loadCredentials = { nil }
+        return keychain
+      }(),
+      httpClient: .unimplemented()
+    )
+
+    do {
+      _ = try await getFileData(.init(fileId: "1234"))
+      XCTFail("Expected to throw, but didn't")
+    } catch {
+      XCTAssertEqual(
+        error as? GetFileData.Error, .notAuthorized,
+        "Expected to throw .notAuthorized, got \(error)"
+      )
+    }
+  }
+}
